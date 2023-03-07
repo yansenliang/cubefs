1efa0f19ced96b88def3f7b764f148fed44de273
wangzhihao <wangzhihao@oppo.com>
2023-03-07 18:49:08 +0800

    feat: add the definition of user route variables
    Signed-off-by: wangzhihao <wangzhihao@oppo.com>

--- objectnode/drive/user_route_mgr.go
+++ objectnode/drive/user_route_mgr.go
@@ -1,20 +1,53 @@
-// Copyright 2023 The CubeFS Authors.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-// implied. See the License for the specific language governing
-// permissions and limitations under the License.
-
 package drive
 
-type userRoute struct{}
+type UserSpace struct {
+	Uid         int    `json:"uid"`
+	ClusterType int8   `json:"clusterType"`
+	ClusterID   int    `json:"clusterId"`
+	VolumeID    int    `json:"volumeId"`
+	DriveID     int    `json:"driveId"`
+	Capacity    uint64 `json:"capacity"`
+	RootPath    string `json:"rootPath"`
+	RootFileID  string `json:"rootFileId"`
+	Ctime       uint64 `json:"ctime"`
+	Params      string `json:"params"`
+}
 
-// TODO user route info store in lru cache
-type userRouteMgr struct{}
+type PathInfo struct {
+	Path   string `json:"path"`
+	Status int8   `json:"status"`
+	Mtime  uint64 `json:"mtime"`
+}
+
+type UserRoute struct {
+	Uid      int        `json:"uid"`
+	AppPaths []PathInfo `json:"appPaths"` //cloud path
+}
+
+type userRouteMgr struct {
+	//TODO user route info store in lru cache
+}
+
+func (d *DriveNode) CreateUserSpace() {
+	//1.Authenticate the token and get the uid
+
+	//2.Applying for space to the cloud service
+
+	//3.Apply to cfs for cluster and volume information
+
+	//4.Locate the user file of the default cluster according to the hash of uid
+
+	//5.Write mappings to extended attributes
+
+	return
+}
+
+func (d *DriveNode) CreateRoute() {
+	//1.Authenticate the token and get the uid
+
+	//2.Get clusterid, volumeid from default cluster
+
+	//3.Store user cloud directory
+
+	return
+}
