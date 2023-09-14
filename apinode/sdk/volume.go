1107eb5a355056d4fb9730bbfd3db01c7ed2908c
Victor1319 <834863182@qq.com>
2023-09-14 16:59:49 +0800

    refactor(sdk): remove subpath param for volume func.
    Signed-off-by: Victor1319 <834863182@qq.com>

--- apinode/sdk/volume.go
+++ apinode/sdk/volume.go
@@ -24,26 +24,26 @@ type IDirSnapshot interface {
 	NewInodeLock() InodeLockApi
 
 	// Lookup from target parentDir ino, parentIno 0 means lookup from root
-	Lookup(ctx context.Context, subPath string, parentIno uint64, name string) (*DirInfo, error)
-	GetInode(ctx context.Context, subPath string, ino uint64) (*proto.InodeInfo, error)
+	Lookup(ctx context.Context, parentIno uint64, name string) (*DirInfo, error)
+	GetInode(ctx context.Context, ino uint64) (*proto.InodeInfo, error)
 	// BatchGetInodes maybe cost much time
-	BatchGetInodes(ctx context.Context, subPath string, parIno uint64, inos []uint64) ([]*proto.InodeInfo, error)
-	Readdir(ctx context.Context, subPath string, parIno uint64, marker string, count uint32) ([]DirInfo, error)
-	ReadDirAll(ctx context.Context, subPath string, ino uint64) ([]DirInfo, error)
-	StatFs(ctx context.Context, subPath string, ino uint64) (*StatFs, error)
-	SetAttr(ctx context.Context, subPath string, req *SetAttrReq) error
+	BatchGetInodes(ctx context.Context, parIno uint64, inos []uint64) ([]*proto.InodeInfo, error)
+	Readdir(ctx context.Context, parIno uint64, marker string, count uint32) ([]DirInfo, error)
+	ReadDirAll(ctx context.Context, ino uint64) ([]DirInfo, error)
+	StatFs(ctx context.Context, ino uint64) (*StatFs, error)
+	SetAttr(ctx context.Context, req *SetAttrReq) error
 	// Mkdir path
-	Mkdir(ctx context.Context, subPath string, parIno uint64, name string) (*InodeInfo, uint64, error)
-	CreateFile(ctx context.Context, subPath string, parentIno uint64, name string) (*InodeInfo, uint64, error)
+	Mkdir(ctx context.Context, parIno uint64, name string) (*InodeInfo, uint64, error)
+	CreateFile(ctx context.Context, parentIno uint64, name string) (*InodeInfo, uint64, error)
 	// Delete dir should be empty before delete
-	Delete(ctx context.Context, subPath string, parIno uint64, name string, isDir bool) error
+	Delete(ctx context.Context, parIno uint64, name string, isDir bool) error
 	Rename(ctx context.Context, srcSubPath, dstSubPath string) error
 
 	// UploadFile file
-	UploadFile(ctx context.Context, subPath string, req *UploadFileReq) (*InodeInfo, uint64, error)
-	WriteFile(ctx context.Context, subPath string, ino, off, size uint64, body io.Reader) error
+	UploadFile(ctx context.Context, req *UploadFileReq) (*InodeInfo, uint64, error)
+	WriteFile(ctx context.Context, ino, off, size uint64, body io.Reader) error
 	// ReadFile read() will make a rpc request to server, if n less than len(data), it means no more data
-	ReadFile(ctx context.Context, subPath string, ino, off uint64, data []byte) (n int, err error)
+	ReadFile(ctx context.Context, ino, off uint64, data []byte) (n int, err error)
 	MultiPart
 	Xattr
 }
@@ -63,12 +63,12 @@ type MultiPart interface {
 }
 
 type Xattr interface {
-	SetXAttr(ctx context.Context, subPath string, ino uint64, key string, val string) error
-	SetXAttrNX(ctx context.Context, subPath string, ino uint64, key string, val string) error
-	BatchSetXAttr(ctx context.Context, subPath string, ino uint64, attrs map[string]string) error
-	GetXAttr(ctx context.Context, subPath string, ino uint64, key string) (string, error)
-	ListXAttr(ctx context.Context, subPath string, ino uint64) ([]string, error)
-	GetXAttrMap(ctx context.Context, subPath string, ino uint64) (map[string]string, error)
-	DeleteXAttr(ctx context.Context, subPath string, ino uint64, key string) error
-	BatchDeleteXAttr(ctx context.Context, subPath string, ino uint64, keys []string) error
+	SetXAttr(ctx context.Context, ino uint64, key string, val string) error
+	SetXAttrNX(ctx context.Context, ino uint64, key string, val string) error
+	BatchSetXAttr(ctx context.Context, ino uint64, attrs map[string]string) error
+	GetXAttr(ctx context.Context, ino uint64, key string) (string, error)
+	ListXAttr(ctx context.Context, ino uint64) ([]string, error)
+	GetXAttrMap(ctx context.Context, ino uint64) (map[string]string, error)
+	DeleteXAttr(ctx context.Context, ino uint64, key string) error
+	BatchDeleteXAttr(ctx context.Context, ino uint64, keys []string) error
 }
