4706c7d5dd156bf19a4589ca51f05005cabf8e80
leonrayang <chl696@sina.com>
2023-12-22 17:21:32 +0800

    update(flashsdk): enable remote cache
    Signed-off-by: leonrayang <chl696@sina.com>

--- proto/admin_proto.go
+++ proto/admin_proto.go
@@ -1111,6 +1111,13 @@ type SimpleVolView struct {
 	LatestVer      uint64
 	Forbidden      bool
 	EnableAuditLog bool
+
+	RemoteCacheBoostEnable   bool
+	RemoteCacheBoostPath     string
+	RemoteCacheAutoPrepare   bool
+	RemoteCacheTTL           int64
+	RemoteCacheReadTimeoutMs int64
+	EnableRemoveDupReq       bool // TODO: using it in metanode
 }
 
 type NodeSetInfo struct {
